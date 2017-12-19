9a6d896217be8efcd3a8fec26dcf9afb19f7ec7a
JacekPolański <jacek.polanski@grupawp.pl>
2017-12-19 16:51:41 +0100

    Add validation to Customer

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -65,6 +65,7 @@ namespace Vidly.Controllers
             return View("MovieForm", viewModel);
         }
 
+        [HttpPost]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -15,6 +15,7 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
@@ -22,6 +23,7 @@ namespace Vidly.Models
         [Display(Name = "Membership Type")]
         public byte MemebershipTypeId { get; set; }
 
+        [Display(Name = "Membership Type")]
         public MembershipType MemebershipType { get; set; }
     }
 }
