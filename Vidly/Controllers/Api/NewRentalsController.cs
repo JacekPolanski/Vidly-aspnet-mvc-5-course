9bc0166aec62bfa71502f4cb51a898ccf6f3cee0
Jacek Polanski <jacek.polanski.91@gmail.com>
2018-03-10 15:13:02 +0100

    Section 10: CreateNewRentals

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
